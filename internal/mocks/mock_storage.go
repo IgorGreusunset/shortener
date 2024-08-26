de29774fa54fbd8b704a779041aabce637023141
IgorFed <greusunset@mail.ru>
2024-08-27 00:53:53 +0700

    update mocks

--- internal/mocks/mock_storage.go
+++ internal/mocks/mock_storage.go
@@ -63,6 +63,20 @@ func (mr *MockRepositoryMockRecorder) CreateBatch(arg0, arg1 interface{}) *gomoc
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreateBatch", reflect.TypeOf((*MockRepository)(nil).CreateBatch), arg0, arg1)
 }
 
+// Delete mocks base method.
+func (m *MockRepository) Delete(arg0 context.Context, arg1 string) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "Delete", arg0, arg1)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// Delete indicates an expected call of Delete.
+func (mr *MockRepositoryMockRecorder) Delete(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Delete", reflect.TypeOf((*MockRepository)(nil).Delete), arg0, arg1)
+}
+
 // GetByID mocks base method.
 func (m *MockRepository) GetByID(arg0 string) (model.URL, bool) {
 	m.ctrl.T.Helper()
